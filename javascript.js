6459c84b2751261bcfb47de524018ea9fb4a3830
user.email <developerjenzii@gmail.com>
2022-12-02 14:25:52 +0100

    Main gameplay flow setup

--- javascript.js
+++ javascript.js
@@ -11,8 +11,23 @@
 
 const GameBoard = (function () {
   'use strict'
-  gameBoard: [X,O,X,O];
+  // initialize an array of length 9 (gameboard squares) , with null values to be able to iterate over.
+  let gameBoard = Array.apply(null, Array(9));
+  let winner = false;
+  let count = 0;
 
+  
+  console.log('lets start this game');
+  
+  while (!winner) {
+    console.log('hello');
+    count ++;
+    count == 9 ? winner = true : winner = false;
+  }
+
+  console.log('end of game, count Xs and Os? ');
+
+  return gameBoard;
 }());
 
 const DisplayController = (function () {
@@ -22,10 +37,10 @@ const DisplayController = (function () {
 
 const Player = (name, state) => {
   const getState = () => {
-
+   let currentState = GameBoard.gameBoard
   }
 
   const setState = () => {
-    
+
   }
 };
