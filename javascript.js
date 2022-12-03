7132ceda6581c3e24f327011bbc005ef1437c366
user.email <developerjenzii@gmail.com>
2022-12-03 01:43:58 +0100

    Save changes - screenwrite not working

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
-const GameBoard = (function () {
+const GameBoard = async function () {
   'use strict'
   // initialize an array of length 9 i.e. gameboard squares , with null values to be able to iterate over.
+  let displayController = DisplayController;
   let gameBoard = Array.apply(null, Array(9));
   let winner = false;
   let count = 0;
-
+  
+  
   
   console.log('lets start this game');
-  
+  function getClick () { 
+    return new Promise ( acc => {
+      Array.from(tiles).forEach(tile => {
+      tile.addEventListener('click', setMark)});
+       displayController.setThisMark;
+       acc();
+      })
+    };
   while (!winner) {
-    console.log('hello');
-    count ++;
+   
+    await getClick();
     // game stops when any player creates a line of 3 equal symbols in any direction. 
     // logic should account for a tie/mutual loss as well. Either checkmate or full board as ending criteria? both?
     count == 9 ? winner = true : winner = false;
@@ -20,24 +29,27 @@ const GameBoard = (function () {
   console.log('end of game, count Xs and Os? ');
 
   return gameBoard;
-}());
+};
 
-const DisplayController = (function () {
+const DisplayController = function () {
   'use strict'
+  let mark = 'x';
  const tiles = document.querySelectorAll('.tile');
  let index = 0;
-  Array.from(tiles).forEach(tile => {
+  let setThisMark = Array.from(tiles).forEach(tile => {
+  tile.addEventListener('click', setMark);
+ // tile.textContent = index.toString();
   index++;
-  tile.addEventListener('click', setColor)
-  tile.textContent = index.toString();
+});
+return { setThisMark, }
+};
 
-  
- });
-
- function setColor() {
- this.style.backgroundColor == 'hotpink' ? this.style.backgroundColor = 'white' : this.style.backgroundColor ='hotpink';
+ function setMark() {
+ //this.style.backgroundColor == 'hotpink' ? this.style.backgroundColor = 'white' : this.style.backgroundColor ='hotpink';
+ this.textContent == '' ? this.textContent = mark : this.textContent = '';
+ mark == 'x' ? mark = 'o' : mark = 'x';
  }
-}());
+
 
 // player object, makes sense to have an even/uneven player to determine flow-sequence? 
 const Player = (name, symbol) => {
@@ -50,4 +62,12 @@ const Player = (name, symbol) => {
   }
 };
 
-const p1 = Player('jeanine', 'x');
+const moduleGame = (async function(){
+  let gameOne = GameBoard;
+
+  gameOne();
+
+//const p1 = Player('jeanine', 'x');
+
+}());
+
