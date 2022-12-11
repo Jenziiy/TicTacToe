8875c71ef03df8b3e4f826f7a25ced6d6fd96ed2
user.email <developerjenzii@gmail.com>
2022-12-11 22:03:37 +0100

    Create dynamic board

--- javascript.js
+++ javascript.js
@@ -1,73 +1,90 @@
-const GameBoard = async function () {
+const ticTacToeModule = (function () {
   'use strict'
-  // initialize an array of length 9 i.e. gameboard squares , with null values to be able to iterate over.
-  let displayController = DisplayController;
-  let gameBoard = Array.apply(null, Array(9));
-  let winner = false;
-  let count = 0;
-  
+  let _winner = false;
+  let _count = 0;
+  let _boardSize = 3;
+  const _tiles = document.querySelectorAll('.tile');
+
+  function _setBoardSize(boardSize) {
+    _boardSize = boardSize
+   }
+
+  function _initializeGame() {
+    _createBoard(_boardSize);
+  };
+
+  function _createBoard() {
+    const board = document.querySelector('.board');
+
+    for(let rowIndex = 0; rowIndex < _boardSize; rowIndex++) {
+      const row = document.createElement('div');
+      row.classList.add('row');
+
+      for (let columnIndex = 0; columnIndex < _boardSize; columnIndex++) {
+        const column = document.createElement('div');
+        column.classList.add('tile','column');
+
+        row.appendChild(column);
+      }
+      board.appendChild(row);
+    }
+  }
   
   
   console.log('lets start this game');
-  function getClick () { 
-    return new Promise ( acc => {
-      Array.from(tiles).forEach(tile => {
-      tile.addEventListener('click', setMark)});
-       displayController.setThisMark;
-       acc();
-      })
-    };
-  while (!winner) {
+  
+  // while (!_winner) {
    
-    await getClick();
-    // game stops when any player creates a line of 3 equal symbols in any direction. 
-    // logic should account for a tie/mutual loss as well. Either checkmate or full board as ending criteria? both?
-    count == 9 ? winner = true : winner = false;
-  }
+  //   getClick();
+  //   // game stops when any player creates a line of 3 equal symbols in any direction. 
+  //   // logic should account for a tie/mutual loss as well. Either checkmate or full board as ending criteria? both?
+  //   _count == 9 ? winner = true : winner = false;
+  // }
 
   // not a reflection of actual gameplay <placeholder>.
   console.log('end of game, count Xs and Os? ');
 
-  return gameBoard;
-};
+  const displayController = {
+    mark: 'x',
+    index: 0,
+    setThisMark: Array.from(_tiles).forEach(tile => {
+    tile.addEventListener('click', setMark);
+  // tile.textContent = index.toString();
+    this.index++;
+    })
+  };
 
-const DisplayController = function () {
-  'use strict'
-  let mark = 'x';
- const tiles = document.querySelectorAll('.tile');
- let index = 0;
-  let setThisMark = Array.from(tiles).forEach(tile => {
-  tile.addEventListener('click', setMark);
- // tile.textContent = index.toString();
-  index++;
-});
-return { setThisMark, }
-};
-
- function setMark() {
- //this.style.backgroundColor == 'hotpink' ? this.style.backgroundColor = 'white' : this.style.backgroundColor ='hotpink';
- this.textContent == '' ? this.textContent = mark : this.textContent = '';
- mark == 'x' ? mark = 'o' : mark = 'x';
- }
+  function setMark() {
+  //this.style.backgroundColor == 'hotpink' ? this.style.backgroundColor = 'white' : this.style.backgroundColor ='hotpink';
+  this.textContent == '' ? this.textContent = mark : this.textContent = '';
+  mark == 'x' ? mark = 'o' : mark = 'x';
+  }
 
 
 // player object, makes sense to have an even/uneven player to determine flow-sequence? 
-const Player = (name, symbol) => {
-  const getState = () => {
-   let currentState = GameBoard.gameBoard
-  }
-
+    const player = (name, symbol) => {
+    const getState = () => {
+    }
   const setState = () => {
-
+  console.log('hello');
   }
+  return setState, getState;
 };
 
-const moduleGame = (async function(){
-  let gameOne = GameBoard;
+  // return { 
+  //   setBoardSize: function() { 
+  //     _setBoardSize(boardSize);
+  //   },
+  //   createBoard: function() {
+  //     _createBoard();
+  //   }
+  
 
-  gameOne();
-
-//const p1 = Player('jeanine', 'x');
-
+  _createBoard();
 }());
 
+
+
+// const p1 = ticTacToeModule.player('jeanine', 'x');
+// p1.getState();
+// p1.setState();
