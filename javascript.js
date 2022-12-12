11828ccb09489e1198f2ca56d7855752bf0584ab
user.email <developerjenzii@gmail.com>
2022-12-12 22:50:25 +0100

    Create functional game

--- javascript.js
+++ javascript.js
@@ -15,8 +15,8 @@ const ticTacToeModule = (function () {
 
       if (_count > 3){
         let scoresArr = Array.from(tiles).map(tile=>tile.textContent);
-           let arrX = [];//scoresArr.filter( item => item == 'x' );
-           let arrO = []; //scoresArr.filter( item => item == 'o' );
+           let arrX = [];
+           let arrO = []; 
           console.log(scoresArr.indexOf('x'));
           for (let i = 0 ; i<scoresArr.length; i++) {
             if ( scoresArr[i] === 'x' ) {
@@ -26,15 +26,27 @@ const ticTacToeModule = (function () {
             }
           }
           if (arrO.length > 2 || arrX.length > 2) {
-            console.log(arrO, 'hallo');
-            if(arrO == [0,1,2] || arrO ==[0,4,8] ||arrO == [2,4,6] ||arrO == [3,4,5] ||arrO == [6,7,8] || arrO ==[0,3,6] 
-              || arrO == [1,4,7] ||arrO == [2,5,8]) {
-              console.log('congrats O won');
+            let pt = document.getElementById('player-turn');
+            arrO = arrO.sort().toString();
+            arrX = arrX.sort().toString();
+            if(arrO.includes('0,1,2') || arrO.includes('0,4,8') ||arrO.includes('2,4,6') ||arrO.includes('3,4,5') 
+            ||arrO.includes('6,7,8') || arrO.includes('0,3,6') || arrO.includes('1,4,7') ||arrO.includes('2,5,8')) {
+              
+              pt.innerText = 'PLAYER O WON!!!! SUPERRAD';
+              setTimeout(() => {
+                Array.from(tiles).forEach(tile => tile.innerText = "" );
+              }, 3000);
             }
-            else if(arrX == [0,1,2] || arrX == [0,4,8] || arrX == [2,4,6] || arrX == [3,4,5] || arrX ==[6,7,8] ||arrX == [0,3,6]
-               || arrX == [1,4,7] || arrX == [2,5,8]) {
+            else if(arrX.includes('0,1,2') || arrX.includes('0,4,8') ||arrX.includes('2,4,6') ||arrX.includes('3,4,5') 
+            ||arrX.includes('6,7,8') || arrX.includes('0,3,6') || arrX.includes('1,4,7') ||arrX.includes('2,5,8')) {
               console.log('congrats X won');
+              pt.innerText = 'PLAYER X WON!!!! SUPER AMAZE';
+              setTimeout(() => {
+                Array.from(tiles).forEach(tile => tile.innerText = '');
+              }, 3000);
             }
+
+          
           }
           console.log(arrO, arrX);
         }
@@ -65,49 +77,13 @@ const ticTacToeModule = (function () {
       board.appendChild(row);
     }
   }
-  
-  
-  console.log('lets start this game');
-  
-  function waitASec() {
-    setTimeout(() => {
-      console.log('hold up eh');
-    }, 1000);
-  }
-   
-   waitASec(); 
-    // game stops when any player creates a line of 3 equal symbols in any direction. 
-    // logic should account for a tie/mutual loss as well. Either checkmate or full board as ending criteria? both?
-    _count == 3 ? _winner = true : _winner = false;
-
-  // not a reflection of actual gameplay <placeholder>.
-  console.log('end of game, count Xs and Os? ');
 
   function setMark(dis) {
-  dis.style.backgroundColor == 'hotpink' ? dis.style.backgroundColor = 'white' : dis.style.backgroundColor ='hotpink';
   dis.textContent == '' ? dis.textContent = _mark : dis.textContent = '';
   _mark == 'x' ? _mark = 'o' : _mark = 'x';
   }
-
-
-// player object, makes sense to have an even/uneven player to determine flow-sequence? 
-    const player = (name, symbol) => {
-    const getState = () => {
-    }
-  const setState = () => {
-  console.log('hello');
-  }
-  return setState, getState;
-};
   
   _createBoard();
   _displayController();
 
-
 })();
-
-
-
-// const p1 = ticTacToeModule.player('jeanine', 'x');
-// p1.getState();
-// p1.setState();
